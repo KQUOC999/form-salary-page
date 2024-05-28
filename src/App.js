df2aedd2254e96dc7679bdaa941b4fd53c041c35
K QUOC <your.email@example.com>
2024-05-28 22:46:43 +0700

    change

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React,{lazy, Suspense} from "react";
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
-
-const MyForm     = lazy( () => import('./routers/pages/form'));
-const Account    = lazy ( () => import('./routers/pages/account'))
+const Account    = lazy ( () => import('./routers/pages/account'));
+const MyForm     = lazy( () => import('./routers/pages/form'))
 
 function App() {
   return (
