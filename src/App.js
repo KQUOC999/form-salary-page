50d5cdb1f646781e3b2242f08fbed375d74f8439
K QUOC <your.email@example.com>
2024-06-09 02:13:16 +0700

    change

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Suspense, useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import MainPage from './component/MainPage';
 import './index.css'
+import Helmet from "helmet";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -18,17 +19,23 @@ function App() {
   }
 
   return (
-    <Router>
-      <Suspense fallback={<div>Loading....</div>}>
-        <Routes>
-          {!isLoggedIn ? (
-            <Route path="/*" element={<Navigate to="/form-salary-page" />} />
-          ) : (
-            <Route path="/*" element={<MainPage />} />
-          )}
-        </Routes>
-      </Suspense>
-    </Router>
+    <>
+      <Helmet>
+        <meta http-equiv="Content-Security-Policy" content="default-src 'self'; style-src 'self' 'unsafe-inline';" />
+        <meta http-equiv="Permissions-Policy" content="fullscreen=(self), geolocation=()" />
+      </Helmet>
+      <Router>
+        <Suspense fallback={<div>Loading....</div>}>
+          <Routes>
+            {!isLoggedIn ? (
+              <Route path="/*" element={<Navigate to="/form-salary-page" />} />
+            ) : (
+              <Route path="/*" element={<MainPage />} />
+            )}
+          </Routes>
+        </Suspense>
+      </Router>
+    </>
   );
 }
 
